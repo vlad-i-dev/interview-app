69847725b334b30784491ef465b34d2ab569ada0
vlad-i-dev <ilchenkovlad916@gmail.com>
2026-02-18 15:17:04 +0100

    add jobs module

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
+import { JobsController } from './jobs/jobs.controller';
+import { JobsService } from './jobs/jobs.service';
 
 @Module({
   imports: [],
-  controllers: [ApiController],
-  providers: [ApiService],
+  controllers: [ApiController, JobsController],
+  providers: [ApiService, JobsService],
 })
 export class ApiModule {}
